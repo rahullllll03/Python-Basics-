eaa43e9ab0175226ec00dacff21f4cd691a45576
Rahul <rahulghorpade0707@gmail.com>
2026-01-27 21:07:19 +0530

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,19 @@
+# Enter the given marks and sort the increament order
+
+marks = []
+
+f1 = int(input("Enter your marks sub 1: "))
+marks.append(f1)
+f2 = int(input("Enter your marks sub 2: "))
+marks.append(f2)
+f3 = int(input("Enter your marks sub 3: "))
+marks.append(f3)
+f4 = int(input("Enter your marks sub 4: "))
+marks.append(f4)
+f5 = int(input("Enter your marks sub 5: "))
+marks.append(f5)
+f6 = int(input("Enter your marks sub 6: "))
+marks.append(f6)
+
+marks.sort()
+print(marks)
