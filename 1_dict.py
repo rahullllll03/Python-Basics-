44f3ae28f0cfb80405282208f7ccd57b9f6a3fde
Rahul <rahulghorpade0707@gmail.com>
2026-01-30 21:22:59 +0530

    Add files via upload

---
+++ 1_dict.py
@@ -0,0 +1,9 @@
+d = {} # empty dictionary
+marks = {
+    "Rahul": 100,
+    "Rohan": 90,
+    "Aryan": 55
+}
+
+print(marks, type(marks))
+print(marks["Rahul"])
---
+++ 2_dict_method.py
@@ -0,0 +1,11 @@
+marks = {
+    "Rahul": 100,
+    "Rohan": 90,
+    "Aryan": 55
+}
+
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+marks.update({"Rohan": 90, "Harsh": 98})
+print(marks)
